20127902057c09b7f0a8b0e6672f3e1cdbf2831b
Bee Yang <bee.yang-1@mnsu.edu>
2017-03-28 21:37:58 +0000

    New Night Owl App Version2

--- NightOwl/config/routes.rb
+++ NightOwl/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
+  resources :courses
+
+  
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'owl_main#index'
+  root 'courses#index'
   
   
 end
--- NightOwl/db/schema.rb
+++ NightOwl/db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320004121) do
+ActiveRecord::Schema.define(version: 20170324044252) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "firstName",  limit: 40
-    t.string   "lastName",   limit: 40
-    t.string   "email",                 default: "", null: false
-    t.string   "password",   limit: 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "courses", force: :cascade do |t|
+    t.string   "ClassName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 
 end
