bb5d5d39eb51d6cc2ce2d89da3d21f3593549df9
Bee Yang <bee.yang-1@mnsu.edu>
2017-03-29 02:46:09 +0000

    omniauth-commit2

---
+++ NightOwlv2/app/controllers/callbacks_controller.rb
@@ -0,0 +1,35 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+    # def facebook
+    #     @user = User.from_omniauth(request.env["omniauth.auth"])
+    #     sign_in_and_redirect @user
+    # end
+    
+    
+    # def twitter
+    #     @user = User.from_omniauth(request.env["omniauth.auth"])
+    #     sign_in_and_redirect @user
+    # end
+    
+    # def github
+    #     @user = User.from_omniauth(request.env["omniauth.auth"])
+    #     sign_in_and_redirect @user
+    # end
+    
+    
+    def all
+        user = User.from_omniauth(request.env["omniauth.auth"])
+        if user.persisted?
+            flash.notice = "Signed in!"
+            sign_in_and_redirect user
+        else
+            session["devise.user_attributes"] = user.attributes
+            redirect_to new_user_registration_url
+        end
+        
+    end
+    alias_method :facebook, :all
+    # alias_method :twitter, :all
+    # alias_method :github, :all
+    # alias_method :digitalocean, :all
+end
+
