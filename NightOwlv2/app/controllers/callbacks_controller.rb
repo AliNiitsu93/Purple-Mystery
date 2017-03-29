ec95f3d018fcfcd0c17e6603924def92bcf7569d
Bee Yang <bee.yang-1@mnsu.edu>
2017-03-29 21:58:27 +0000

    update user's notice when sign in through provider

--- NightOwlv2/app/controllers/callbacks_controller.rb
+++ NightOwlv2/app/controllers/callbacks_controller.rb
@@ -19,7 +19,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
     def all
         user = User.from_omniauth(request.env["omniauth.auth"])
         if user.persisted?
-            flash.notice = "Signed in!"
+            flash.notice = "You are signed in successfully with #{user.provider}!"
             sign_in_and_redirect user
         else
             session["devise.user_attributes"] = user.attributes
