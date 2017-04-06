590488eb7d7ea360a61ca044a6c05805a7af9c08
Bee Yang <bee.yang-1@mnsu.edu>
2017-04-06 19:59:18 +0000

    Updates to UI and login display error

--- NightOwlV3.1/db/schema.rb
+++ NightOwlV3.1/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404222021) do
+ActiveRecord::Schema.define(version: 20170406183243) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170404222021) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
