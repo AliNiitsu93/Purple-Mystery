f9b4394c181f9dcab4f229fbdfc8bfb9d3cca15c
Bee Yang <bee.yang-1@mnsu.edu>
2017-04-06 21:08:32 +0000

    Updates

--- NightOwlV3.1/config/routes.rb
+++ NightOwlV3.1/config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-# <<<<<<< JanojBranch
-# #   get 'users/index'
 
-# #   devise_for :users
-# #   resources :users
-# =======
-
-
-
-# >>>>>>> master
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :messages do 
   	resources :comments
